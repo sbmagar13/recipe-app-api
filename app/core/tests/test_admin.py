8c3b71efb4c8d06aa6c448e3a21af0039ff147fc
SBMagar <sbmagar.sbm99@gmail.com>
2020-08-11 19:58:10 +0545

    Updated admin to support custom user model

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -12,16 +12,31 @@ class AdminSiteTests(TestCase):
             password='password123'
             )
         self.client.force_login(self.admin_user)
-        self.user = get_user_model().objects.creat_usser(
+        self.user = get_user_model().objects.create_user(
             email='test@gmail.com',
             password='password123',
             name='Test user full name'
             )
 
-        def test_users_listed(self):
-            """Test that users are listed on user page"""
-            url = reverse('admin:core_user_changelist')
-            res = self.client.get(url)
+    def test_users_listed(self):
+        """Test that users are listed on user page"""
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
 
-            self.assertContains(res, self.user.name)
-            self.assertContains(res, self.user.email)
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
+
+    def test_user_change_page(self):
+        """Test that the user edit page works"""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        # /admin/core/user/1 this is how args works in reverse function
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test that the creat user page works"""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
