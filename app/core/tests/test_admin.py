706c3ca6087e2378bfb4865750e4a6435c33cbb4
SBMagar <sbmagar.sbm99@gmail.com>
2020-08-11 17:08:31 +0545

    indentations

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -5,23 +5,23 @@ from django.urls import reverse
 
 class AdminSiteTests(TestCase):
 
-	def setUp(self):
-		self.client = Client()
-		self.admin_user = get_user_model().objects.create_superuser(
-			email='admin@gmail.com',
-			password='password123'
-			)
-		self.client.force_login(self.admin_user)
-		self.user = get_user_model().objects.creat_usser(
-			email='test@gmail.com',
-			password='password123',
-			name='Test user full name'
-			)
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@gmail.com',
+            password='password123'
+            )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.creat_usser(
+            email='test@gmail.com',
+            password='password123',
+            name='Test user full name'
+            )
 
-		def test_users_listed(self):
-			"""Test that users are listed on user page"""
-			url = reverse('admin:core_user_changelist')
-			res = self.client.get(url)
+        def test_users_listed(self):
+            """Test that users are listed on user page"""
+            url = reverse('admin:core_user_changelist')
+            res = self.client.get(url)
 
-			self.assertContains(res, self.user.name)
-			self.assertContains(res, self.user.email)
+            self.assertContains(res, self.user.name)
+            self.assertContains(res, self.user.email)
