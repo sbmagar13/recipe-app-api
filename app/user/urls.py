961896de0c8621378d63992be89bc7cf91a1e90b
SBMagar <sbmagar.sbm99@gmail.com>
2020-08-12 23:26:24 +0545

    Added endpoint for creating and authenticating users

--- app/user/urls.py
+++ app/user/urls.py
@@ -7,5 +7,5 @@ app_name = 'user'
 
 urlpatterns = [
     path('create/', views.CreateUserView.as_view(), name='create'),
-    # path('', views.)
+    path('token/', views.CreateTokenView.as_view(), name='token'),
 ]
